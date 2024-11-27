3c75fa9bcd2b5368ea86bca7f26b1e25d236eeee
Valentina Sinichenko <valentyna.sinichenko@mailbox.tu-dresden.de>
2024-11-27 22:33:25 +0100

    EDA, explore anomalies, fix 1st appeariing of bikes in the dataset, fix some package versions, conda init zsh

---
+++ tools/my_tools.py
@@ -0,0 +1,15 @@
+
+from datetime import datetime
+import pyreadr
+
+
+def get_filename(time):
+    return f'../data/Dresden_01-03.24/{time.strftime("%Y-%m-%d")}/{time.strftime("%Y-%m-%d-%H-%M-00")}.rds'
+
+
+
+def import_raw_file(year,month, day, hour, minute, second):
+    time = datetime(year,month, day, hour, minute, second)
+    current_filename = get_filename(time)
+    df = pyreadr.read_r(current_filename)[None]
+    return df
