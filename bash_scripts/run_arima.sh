dfd4fbf681a5e7285fd39f8a3631b13d210f27d3
Valentina Sinichenko <valentyna.sinichenko@mailbox.tu-dresden.de>
2025-03-21 14:17:53 +0100

    hpc: allocate more CPU memory

--- bash_scripts/run_arima.sh
+++ bash_scripts/run_arima.sh
@@ -2,6 +2,7 @@
 
 #SBATCH --ntasks=1                   # #SBATCH lines request resources and
 #SBATCH --cpus-per-task=4                 # #SBATCH lines request resources and
+#SBATCH --mem=200G
 #SBATCH --time=01:00:00               # specify Slurm options
 #SBATCH --job-name=arima_one_hexagon           # All #SBATCH lines have to follow uninterrupted
 #SBATCH --output=tmp/simulation-%j.out    # after the shebang line
