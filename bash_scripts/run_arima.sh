87bb04e3f50a69fdc8fefe8fa8616da02fde3f1e
Valentina Sinichenko <valentyna.sinichenko@mailbox.tu-dresden.de>
2025-03-26 11:00:11 +0100

    hpc: more tiime and resources

--- bash_scripts/run_arima.sh
+++ bash_scripts/run_arima.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH --ntasks=1                   # #SBATCH lines request resources and
-#SBATCH --cpus-per-task=20                 # #SBATCH lines request resources and
+#SBATCH --cpus-per-task=30                 # #SBATCH lines request resources and
 #SBATCH --mem=300G
-#SBATCH --time=05:00:00               # specify Slurm options
+#SBATCH --time=10:00:00               # specify Slurm options
 #SBATCH --job-name=sarima_all_multi          # All #SBATCH lines have to follow uninterrupted
 #SBATCH --output=tmp/sarima_all_multi-%j.out    # after the shebang line
 #SBATCH --error=tmp/sarima_all_multi-%j.err     # Comments start with # and do not count as interruptions
