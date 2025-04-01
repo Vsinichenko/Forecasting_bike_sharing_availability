d9937a1f868b44c82a48283d4f7ff2df924a4675
Valentina Sinichenko <valentyna.sinichenko@mailbox.tu-dresden.de>
2025-04-01 14:53:59 +0200

    hpc: fix

--- bash_scripts/run_arima.sh
+++ bash_scripts/run_arima.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=100                
 #SBATCH --mem=100G
 #SBATCH --time=10:00:00              
-#SBATCH --job-name=sarimax_calendar_          
+#SBATCH --job-name=sarimax_calendar_FB          
 #SBATCH --output=logs/sarimax_calendar_FB-%j.out    
 #SBATCH --error=logs/sarimax_calendar_FB-%j.err     
 
