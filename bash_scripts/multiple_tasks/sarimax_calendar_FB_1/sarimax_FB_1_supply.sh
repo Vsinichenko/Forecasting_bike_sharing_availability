62afab7352f5c099ce39c0c9be9e9932ac0fa33f
Valentina Sinichenko <valentyna.sinichenko@mailbox.tu-dresden.de>
2025-04-06 17:43:01 +0200

    hpc: 2 scripts to rerun

---
+++ bash_scripts/multiple_tasks/sarimax_calendar_FB_1/sarimax_FB_1_supply.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1                  
+#SBATCH --cpus-per-task=100                
+#SBATCH --mem=10G
+#SBATCH --time=10:00:00              
+#SBATCH --job-name=FB_1_supply          
+#SBATCH --output=logs/sarimax_calendar_FB_1_supply_-%j.out    
+#SBATCH --error=logs/sarimax_calendar_FB_1_supply_-%j.err     
+
+source /home/vasi018e/miniconda3/etc/profile.d/conda.sh
+
+conda activate FBSfor_arima
+
+python "python_scripts/16f_SARIMAX_calendar_after_FB_interpolation.py"  --city FB --part 1 --depvar supply
